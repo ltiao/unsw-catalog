6cc1a0f822680059f3aa0e58b4504dd912dfaaef
Louis Tiao <louistiao@gmail.com>
2014-08-01 01:44:14 +1000

    added more models

--- unsw_catalog/catalog/models.py
+++ unsw_catalog/catalog/models.py
@@ -19,7 +19,8 @@ class Course(models.Model):
     school = models.CharField(max_length=100)
     campus = models.CharField(max_length=30)
     gened = models.BooleanField(
-        verbose_name = 'General Education', 
+        verbose_name = 'General Education',
+        help_text = 'Available for General Education',
         default = False
     )
     description_markup = models.TextField(verbose_name='Description (markup)')
@@ -46,19 +47,37 @@ class Course(models.Model):
 
     #TODO: normalize description
 
-# risky class definition here... seems to work though :/
 class Class(models.Model):
-    course = models.ForeignKey(Course)
-    class_nbr = models.PositiveIntegerField()
+    course = models.ForeignKey(Course, related_name='classes')
+    class_nbr = models.PositiveIntegerField(unique=True)
     activity = models.CharField(max_length=25)
     section = models.CharField(max_length=5)
+    teaching = models.CharField(verbose_name='Teaching Period', max_length=50)
+    status = models.CharField(max_length=10)
+    enrolments = models.PositiveIntegerField()
+    capacity = models.PositiveIntegerField()
+    offering_start = models.DateField()
+    offering_end = models.DateField()
+    census_date = models.DateField()
+    mode = models.CharField(verbose_name='Instruction Mode', max_length=30)
+    consent = models.CharField(max_length=30)
     accessed = models.DateTimeField(
         verbose_name = 'Last Accessed',
         help_text = 'Last time the source was accessed by crawler', 
         auto_now = True
     )
     updated = models.DateTimeField(
-        verbose_name = 'Updated',
+        verbose_name = 'Last Updated',
         help_text = 'Last time source itself was updated',
     )
     url = models.URLField(max_length=80)
+
+class Meeting(models.Model):
+    # need to add a French twist to the field name
+    classe = models.ForeignKey(Class, verbose_name='Class', related_name='meetings')
+    day = models.CharField(max_length=5)
+    time_start = models.CharField(max_length=5)
+    time_end = models.CharField(max_length=5)
+    location = models.CharField(max_length=100)
+    weeks = models.CharField(max_length=20)
+    instructor = models.CharField(max_length=50)
